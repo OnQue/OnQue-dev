8c6c2f38862581a34bf9373fdfe9c0e57923d363
Vikas Mishra <vikasmishra95@gmail.com>
2014-12-09 22:48:50 +0530

    Updated admin.py to include editing of status, waiting list

--- clients/admin.py
+++ clients/admin.py
@@ -11,7 +11,7 @@ class RecordAdmin(admin.ModelAdmin):
 class tableAdmin(admin.ModelAdmin):
     list_display = ('user','mobile', 'rest_name', 'n_of_table','status','waiting_list','seated','first_login')
     search_fields = ('user__username','rest_name')
-    fields = ('rest_name','n_of_table','first_login')
+    fields = ('rest_name','n_of_table','first_login','status','waiting_list','seated')
 
 class FeedbackAdmin(admin.ModelAdmin):
 	list_display = ('user','mobile','date','service','ambience','food','overall_exp','staff_friend')
